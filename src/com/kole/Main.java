3a30cd0fc3e6a06ce7cb21744fb38f68ed86735a
Slobodan <slobodan.kosanovic@gmail.com>
2018-03-27 19:38:06 +0200

    Initial commit

---
+++ src/com/kole/Main.java
@@ -0,0 +1,15 @@
+package com.kole;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u06B7';
+        System.out.println("Unicode output was " + myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+
+        char myNewChar = '\u00AE';
+        System.out.println("My newChar is " + myNewChar);
+    }
+}
